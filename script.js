1f7b0c0c666fe64b29831d2e768f1a283c16c1e8
Brobo1 <armanahz95@gmail.com>
2024-06-17 22:34:29 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const btns = document.querySelectorAll("button");
+
+const opts = {
+  rock: { beats: "scissors", loses: "paper" },
+  paper: { beats: "rock", loses: "scissors" },
+  scissors: { beats: "paper", loses: "rock" },
+};
+
+let humanScore = 0;
+let compScore = 0;
+
+const rand = () => {
+  const compOpt = ["rock", "paper", "scissors"];
+  const random = Math.floor(Math.random() * compOpt.length);
+  return compOpt[random];
+};
+
+const playRound = (player) => {
+  const comp = rand();
+  console.log(player, comp);
+  if (opts[player].beats === comp) {
+    humanScore++;
+    return "player won";
+  } else if (opts[comp].beats === player) {
+    compScore++;
+    return "computer won";
+  } else {
+    return "draw";
+  }
+};
+
+btns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    console.log(playRound(btn.id));
+    console.log(humanScore, compScore);
+  }),
+);
